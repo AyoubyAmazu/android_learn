66024efbb977148f3040a53ee365d7cfc7d6f621
AyoubyAmazu <ayoubamazu@gmail.com>
2024-10-08 09:54:16 +0100

    tuto done

---
+++ tuto/tuto1.2.kt
@@ -0,0 +1,19 @@
+fun tutorials.main() {
+    val number: Int = 5
+    println(number)
+    val name: String = "ayoub"
+    println(number)
+    val number1: Double = 1234.2344
+    println(number1)
+    val bool: boolean = true
+    println(bool)
+    println("$number + $number1 = ${number + number1}")
+}
+
+fun tutorials.main() {
+    var count: Int = 5
+    count = count + 1
+    count++
+    println(count)
+
+}
---
+++ tuto/tuto1.4.kt
@@ -0,0 +1,7 @@
+fun tutorials.main() {
+    fun birthdayGreeting(name: String = "Rover", age: Int): String {
+        return "Happy Birthday, $name! You are now $age years old!"
+    }
+    println(birthdayGreeting(age = 5))
+    println(birthdayGreeting("Rex", 2))
+}
